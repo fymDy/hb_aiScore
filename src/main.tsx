733ff9df03cdd1731f0257a574b7ba8deae88391
mark <mark995@gmail.com>
2025-04-02 19:48:46 +0400

    去掉.scss之间文件相互引用，因为@import 在sass@3.0中将废弃，sass@1.8中终端会输出警告

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import '@/assets/styles/normalize8.css'
 import '@/assets/styles/global.scss'
+import '@/assets/styles/theme.scss'
 import '@/assets/styles/index.scss'
 
 createRoot(document.getElementById('root')!).render(
