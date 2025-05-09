11ea3ebbc01d359b98ebb50ecf93760e89d0e092
mark <mark995@gmail.com>
2025-05-09 21:41:57 +0400

    images 加入默认图像

--- src/components/Common/Images/index.tsx
+++ src/components/Common/Images/index.tsx
@@ -1,13 +1,14 @@
 /*
  * @Author: Mark
  * @Date: 2025-04-27 20:19:26
- * @LastEditTime: 2025-05-08 13:46:44
+ * @LastEditTime: 2025-05-09 21:41:26
  * @LastEditors: MarkMark
  * @Description: 佛祖保佑无bug
  * @FilePath: /hb_aiScore/src/components/Common/Images/index.tsx
  */
 import styles from './index.module.scss'
 import cs from 'classnames'
+import defaultPlayer from '@/assets/images/default-player.png'
 const Image:React.FC<{
     imgSrc:any,
     imgStyle?:any,
@@ -17,9 +18,13 @@ const Image:React.FC<{
     imgStyle,
     className,
 })=>{
+    const handleImgError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+        e.currentTarget.onerror = null; // 防止 fallback 图也报错死循环
+        e.currentTarget.src = defaultPlayer;
+      };
     return(
        <div className={cs(className,styles.Image)}>
-            <img style={imgStyle} src={imgSrc} />
+            <img style={imgStyle} src={imgSrc?imgSrc:defaultPlayer} onError={handleImgError} />
       </div>
     )
 }
