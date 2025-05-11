c2d015ac5fbe8a6595e99ec3e0248c7cf6b0c4bc
mark <mark995@gmail.com>
2025-05-11 12:10:01 +0400

    积分榜-联赛

--- src/views/matchDetails/table/league/index.tsx
+++ src/views/matchDetails/table/league/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: Mark
  * @Date: 2025-05-10 19:27:45
- * @LastEditTime: 2025-05-10 20:34:56
+ * @LastEditTime: 2025-05-11 11:56:09
  * @LastEditors: MarkMark
  * @Description: 佛祖保佑无bug
  * @FilePath: /hb_aiScore/src/views/matchDetails/table/league/index.tsx
@@ -11,7 +11,8 @@ import PlayerComp from '@/components/PlayerComp';
 import Title from '../../_components/title';
 import {  StandingTeam } from '../enum';
 import styles from './index.module.scss'
-
+import cs from 'classnames'
+import Images from '@/components/Common/Images';
 const League: React.FC<{
   data: StandingTeam[];
 }> = ({
@@ -19,33 +20,33 @@ const League: React.FC<{
 }) => {
 return (
     <section className={styles.League}>
-      <Title title={'聯賽 '} isMore={false}/>
+      <Title className={styles.Title} title={'聯賽 '} isMore={false}/>
         <div className={styles.table}>
             <div className={styles.tableHeader}>
-              <span>排名</span>
-              <span>队伍</span>
-              <span>P</span>
-              <span>W</span>
-              <span>D</span>
-              <span>L</span>
-              <span>進球</span>
-              <span>净胜</span>
+              <label className={styles.order}>#</label>
+              <div className={styles.PlayerComp}  >队伍</div>
+              <span className={styles.P}>P</span>
+              <span className={styles.W}>W</span>
+              <span className={styles.D}>D</span>
+              <span className={styles.L}>L</span>
+              <label className={styles.goals}>進球</label>
+              <span>±</span>
               <span>Pts</span>
             </div>
-            <div>
+            <div className={styles.tableContent}>
             {data.map((team:StandingTeam, index:number) => (
-              <div key={index} className={styles[team.status || 'normal']}>
-                <span>{team.rank}</span>
+              <div key={index} className={cs(styles.tableRow,styles[team.status || 'normal'])}>
+                <label  className={styles.order}>{team.rank}</label>
                 <PlayerComp className={styles.PlayerComp}  logo={team.team_logo} name= {team.team_name}/>
-                <span>{team.match_played}</span>
-                  <span>{team.wins}</span>
-                <span>{team.draws}</span>
-                <span>{team.losses}</span>
-                <span>{team.goals_for}-{team.goals_against}</span>
-                <span>{team.goal_diff}</span>
-                <span>{team.points}</span>
+                <span className={styles.P}>{team.match_played}</span>
+                <span className={styles.W}>{team.wins}</span>
+                <span className={styles.D}>{team.draws}</span>
+                <span className={styles.L}>{team.losses} </span>
+                <label className={styles.goals}>{team.goals_for}-{team.goals_against}</label>
+                <span className={styles.goal_diff}>{team.goal_diff}</span>
+                <span className={styles.pts}>{team.points}</span>
               </div>
-          ))}
+            ))}
           </div>
           </div>
             
